35d5a0c6527fc316b0fe506e912dae0ab58a928c
agentlogs01-art <agentlogs01@gmail.com>
2026-06-26 00:01:34 +0530

    Delete gunicorn.conf.py

--- gunicorn.conf.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-# Force the /app root into the Python search space inside Gunicorn's core process
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
-
-# Standard production binding configurations
-bind = "0.0.0.0:" + os.getenv("PORT", "8080")
-workers = 2
